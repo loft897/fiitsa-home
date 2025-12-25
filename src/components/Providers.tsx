629b5837207dcb12c759637d93e9b8c0e94f3660
Galus Fotso <galusfotso184@gmail.com>
2025-12-25 20:49:32 +0100

    tracker global

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 import { LanguageProvider } from "@/hooks/useLanguage";
@@ -9,7 +10,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <LanguageProvider>
-        <GlobalTracker />
+        <Suspense fallback={null}>
+          <GlobalTracker />
+        </Suspense>
         {children}
         <Toaster richColors position="top-right" />
       </LanguageProvider>
